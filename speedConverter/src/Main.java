79f605dc05f0fe7e890ec2d5745d881809027843
Simon-Moseley <simon.moseley.dev@gmail.com>
2020-05-14 21:49:43 -0500

    Initial commit

---
+++ speedConverter/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        long miles = speedConverter.toMilesPerHour(10.5);
+        System.out.println("miles = " + miles);
+
+        speedConverter.printConversion(10.5);
+    }
+}
